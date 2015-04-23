6eb56a02ceb6ef810b9715cfb0a0a4ad087c45a1
Dan Stepanov <danhstepanov@gmail.com>
2015-04-23 00:04:39 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+jQuery(function($) {'use strict',
+
+	//Countdown js
+	$("#countdown").countdown({
+			date: "20 april 2015 6:00:00",
+			format: "on"
+	},
+		
+	function() {
+			// callback function
+	});
+});
